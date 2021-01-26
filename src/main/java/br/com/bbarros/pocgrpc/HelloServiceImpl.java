d07cb572e21e77e3eb14b5c12e91ac5179edde74
Bruno Barros <brunobarros@ZUP-2826.local>
2021-01-26 20:46:56 -0300

    Criando o servidor de gRPC

---
+++ src/main/java/br/com/bbarros/pocgrpc/HelloServiceImpl.java
@@ -0,0 +1,22 @@
+package br.com.bbarros.pocgrpc;
+
+import io.grpc.stub.StreamObserver;
+
+public class HelloServiceImpl extends HelloServiceGrpc.HelloServiceImplBase {
+    @Override
+    public void hello(HelloRequest request, StreamObserver<HelloResponse> responseObserver) {
+        String greeting = new StringBuilder()
+                .append("Hello, ")
+                .append(request.getFirstName())
+                .append(" ")
+                .append(request.getLastName())
+                .toString();
+
+        HelloResponse response = HelloResponse.newBuilder()
+                .setGreeting(greeting)
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}
